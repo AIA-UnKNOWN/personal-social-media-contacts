6fb186ae7ce1ab1b7465adf5d7db5c47525d5d10
Ajboy Ian Abordo <ajboyian321@gmail.com>
2023-12-07 00:13:54 +0800

    created home page UI

--- pages/home.php
+++ pages/home.php
@@ -11,17 +11,75 @@
     <?php
     session_start();
 
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        session_destroy();
-        header('Location: signin.php');
-    }
+    // if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    //     session_destroy();
+    //     header('Location: signin.php');
+    // }
     ?>
 
-    <h1>Welcome <?= $_SESSION['username'] ?>!</h1>
-
-    <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
-        <input class="cursor-pointer" type="submit" name="logout" value="Logout">
-    </form>
+    <div class="min-h-screen pb-[50px]">
+        <div class="min-h-[200px] bg-[#E9E9E9]"></div>
+        <div class="h-[200px] w-[200px] rounded-full mx-auto -mt-[100px] overflow-hidden shadow-lg">
+            <img
+                src="https://avatars.githubusercontent.com/u/63773715?s=400&u=08ff2e23aff8f7d9d331732d3f393a4036a74cfd&v=4"
+                alt="Ajboy Ian Abordo picture"
+            />
+        </div>
+        <div class="flex justify-center mt-[65px] mb-[53px]">
+            <form
+                action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>"
+                method="post"
+                class="p-[25px] rounded-[10px] border-[1px] border-[#EAEAEA] w-[500px]"
+            >
+                <div class="flex flex-col gap-[10px]">
+                    <div>
+                        <label
+                            for="platform-name"
+                            class="block mb-[10px]"
+                        >Platform Name</label>
+                        <input
+                            type="text"
+                            name="platform-name"
+                            id="platform-name"
+                            class="block w-full bg-[#E9E9E9] h-[40px] rounded-[10px] px-[10px] outline-none"
+                            autofocus
+                        >
+                    </div>
+                    <div>
+                        <label
+                            for="link"
+                            class="block mb-[10px]"
+                        >Link</label>
+                        <input
+                            type="text"
+                            name="link"
+                            id="link"
+                            class="block w-full bg-[#E9E9E9] h-[40px] rounded-[10px] px-[10px] outline-none"
+                        >
+                    </div>
+                </div>
+                <div class="mt-[40px]">
+                    <input
+                        type="submit"
+                        name="add"
+                        value="Add"
+                        class="block w-full bg-[#6D96FF] text-white h-[40px] rounded-[10px] px-[10px] outline-none"
+                    >
+                </div>
+            </form>
+        </div>
+        <p class="text-center text-[32px] font-[700] mb-[35px]">Social Media Links</p>
+        <ul class="flex justify-center items-center flex-wrap gap-[20px] max-w-[950px] mx-auto">
+            <?php for ($i = 1; $i <= 14; $i++) { ?>
+                <li class="min-w-[230px] rounded-[10px] bg-[#E9E9E9]">
+                    <a
+                        href="platform/<?= $i ?>"
+                        class="flex justify-center items-center min-h-[40px] w-full"
+                    >Platform <?= $i ?></a>
+                </li>
+            <?php } ?>
+        </ul>
+    </div>
     
 </body>
 </html>
